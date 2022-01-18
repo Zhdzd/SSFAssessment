bd460c0c1f063380c7732f925df6fef6e89bd778
Zhdzd <zahidabdullah18@gmail.com>
2022-01-18 09:15:14 +0800

    initial commit

---
+++ src/test/java/com/example/SSFAssessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSFAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
