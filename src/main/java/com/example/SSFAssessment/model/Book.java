bf617534acc8cdc8d58304ff539acc2aea0a29a2
Zhdzd <zahidabdullah18@gmail.com>
2022-01-18 10:35:51 +0800

    commit

---
+++ src/main/java/com/example/SSFAssessment/model/Book.java
@@ -0,0 +1,9 @@
+package com.example.SSFAssessment.model;
+
+public class Book {
+    private String title;
+    private String description;
+    private String excerpt;
+    private boolean cached;
+    private String bookCover;
+}
--- src/main/java/com/example/SSFAssessment/service/BookService.java
+++ src/main/java/com/example/SSFAssessment/service/BookService.java
@@ -1,5 +1,37 @@
 package com.example.SSFAssessment.service;
 
+import java.util.List;
+
+import com.example.SSFAssessment.model.Book;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 public class BookService {
-    
+    private static final Logger logger = LoggerFactory.getLogger(BookService.class);
+    private String urlSearch = "http://openlibrary.org/search.json";
+
+
+    public List<Book> search (String searchTerm){
+        
+        String url = UriComponentsBuilder
+            .fromUriString(urlSearch)
+            .queryParam("title", searchTerm.trim().replace(" ","+"))
+            .queryParam("key", )
+            .queryParam("limit", 20)
+            .toUriString();
+
+        logger.info("url: " +url);
+
+        RequestEntity<Void> req = RequestEntity.get(url).build();
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> resp = template.exchange(req, String.class);
+        
+
+
+    }
 }
