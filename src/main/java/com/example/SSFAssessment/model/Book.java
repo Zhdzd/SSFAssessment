7a87c29fcf10812b99476ffdeab7c4b97d3e4cd1
Zhdzd <zahidabdullah18@gmail.com>
2022-01-18 10:58:29 +0800

    commit

--- src/main/java/com/example/SSFAssessment/model/Book.java
+++ src/main/java/com/example/SSFAssessment/model/Book.java
@@ -6,4 +6,38 @@ public class Book {
     private String excerpt;
     private boolean cached;
     private String bookCover;
+
+ 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
+    }
+
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+    public boolean isCached() {
+        return cached;
+    }
+
+    public void setCached(boolean cached) {
+        this.cached = cached;
+    }
+
 }
--- src/main/java/com/example/SSFAssessment/service/BookService.java
+++ src/main/java/com/example/SSFAssessment/service/BookService.java
@@ -1,15 +1,23 @@
 package com.example.SSFAssessment.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import com.example.SSFAssessment.model.Book;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 
 public class BookService {
     private static final Logger logger = LoggerFactory.getLogger(BookService.class);
@@ -21,15 +29,26 @@ public class BookService {
         String url = UriComponentsBuilder
             .fromUriString(urlSearch)
             .queryParam("title", searchTerm.trim().replace(" ","+"))
-            .queryParam("key", )
-            .queryParam("limit", 20)
+            .queryParam("fields", "key+title")
+            .queryParam("limit", "20")
             .toUriString();
 
         logger.info("url: " +url);
-
         RequestEntity<Void> req = RequestEntity.get(url).build();
+
         RestTemplate template = new RestTemplate();
         ResponseEntity<String> resp = template.exchange(req, String.class);
+
+        if(resp.getStatusCode() !=HttpStatus.OK)
+            throw new IllegalArgumentException("Error");
+
+            String body = resp.getBody();
+        try(InputStream is = new ByteArrayInputStream(body.getBytes())){
+            JsonReader reader = Json.createReader(is);
+            JsonObject data = reader.readObject();
+            JsonArray searchResults = data.getJsonArray("");
+          
+        }catch (Exception ex){}
         
 
 
